ff861c540b0eaf4744a5c9ee92df3b86a2a68f5b
aristidebamenou <aristide.bamenou@hotmail.com>
2022-08-14 14:15:14 +0200

    feature: components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,75 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { ContactComponent } from './contact/contact.component';
+import { SessionsComponent } from './sessions/sessions.component';
+import { SessionDetailsComponent } from './session-details/session-details.component';
+import { PricesComponent } from './prices/prices.component';
+import { HouseRulesComponent } from './house-rules/house-rules.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProgramComponent } from './program/program.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatTreeModule} from "@angular/material/tree";
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sessions', component: SessionsComponent },
+  { path: 'sessionDetails', component: SessionDetailsComponent },
+  { path: 'prices', component: PricesComponent },
+  { path: 'houseRules', component: HouseRulesComponent },
+  { path: 'program', component: ProgramComponent },
+  { path: 'subscription', component: SubscriptionComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    PostComponent,
+    ContactComponent,
+    SessionsComponent,
+    SessionDetailsComponent,
+    PricesComponent,
+    HouseRulesComponent,
+    BlogComponent,
+    ProgramComponent,
+    SubscriptionComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(routes),
+    MatTabsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/house-rules/house-rules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-house-rules',
+  templateUrl: './house-rules.component.html',
+  styleUrls: ['./house-rules.component.scss']
+})
+export class HouseRulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
